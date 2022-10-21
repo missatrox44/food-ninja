9f54ddc421bad2a83d0607013223357f67d0a8da
Sara Baqla <missatrox44@gmail.com>
2022-10-21 12:42:56 -0500

    created custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/index.html',
-  ],
-  // ...
+  content: ['./public/index.html',],
+  theme: {
+    //create personalized colors, extension of tailwind out the box
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
+  },
+  plugins: [],
 }
